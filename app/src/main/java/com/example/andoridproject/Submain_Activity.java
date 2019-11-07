fde9729b662f3bf17ee0adeaa6f49838f81d0482
ChanYangYu <clsrnthd7@gmail.com>
2019-11-07 12:49:40 +0900

    11.7(수정) Activity가 여러개로 다운로드 되는문제 해결(Launcher Activity 체크해제) 간단하게 SubActivity xml구성

--- app/src/main/java/com/example/andoridproject/Submain_Activity.java
+++ app/src/main/java/com/example/andoridproject/Submain_Activity.java
@@ -3,9 +3,9 @@ package com.example.andoridproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class Submain_Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
