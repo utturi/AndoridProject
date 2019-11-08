457efe7977e98162283e108ecf15b835fb6712e3
ChanYangYu <clsrnthd7@gmail.com>
2019-11-09 01:12:27 +0900

    11.8 수정 - 사용자의 직접입력에 대한 간단한 db구성(delete미구현) - subactivity를 이용한 동적 textview생성 실패!   (subactivity textview요소가 invisable로 생성되어있고    데이터가 들어오면 순서대로 visiable시켜서 보이게 하려고함.) - 임시로 mainbutton TextView로 변경(추후에 버튼으로 변경 예정)

--- app/src/main/java/com/example/andoridproject/MainActivity.java
+++ app/src/main/java/com/example/andoridproject/MainActivity.java
@@ -5,27 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button mainbut; //음식이름 뜨는 큰 화면
+    TextView mainbut; //음식이름 뜨는 큰 화면
     ImageButton directbut; //직접등록버튼
     DatePickerDialog mDialog; //달력사용
     String TAG = "FoodName"; //직접등록 다이얼로그
+    String name;           //음식이름
     ImageButton soundbut; //사운드 버튼
+    SQLiteDatabase database; //데이터베이스
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.testButton);
+        mainbut = findViewById(R.id.mainbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectData("Food");
+            }
+        });
 
         //직접등록버튼 입력 -> 달력(유통기한설정)
         directbut = findViewById(R.id.directbutton);
@@ -45,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
                         Log.v(TAG, "Yes Btn Click");
 
                         //Text값 받아서 로그 남기기
-                        String value = et.getText().toString(); //음식입력받은 값 저장
-                        Log.v(TAG, value);
+                        name = et.getText().toString(); //음식입력받은 값 저장
+                        Log.v(TAG, name);
 
                         dialog.dismiss(); //닫기
                         mDialog.show(); //달력창 띄우기
@@ -70,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         mDialog = new DatePickerDialog(this, listener, 2019, 11, 8);
 
         //SubActivity로 넘어가는 버튼
-        mainbut = findViewById(R.id.mainbutton);
+       // mainbut = findViewById(R.id.mainbutton);
         mainbut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,7 +106,53 @@ public class MainActivity extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            Toast.makeText(getApplicationContext(), year + "년" + monthOfYear + "월" + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
+            openDatabase("Food.db");
+            createTable("Food");
+            insertData(name, year+"-"+monthOfYear+"-"+dayOfMonth,"Food");
+            mainbut.setText(name+"\n"+year+"-"+monthOfYear+"-"+dayOfMonth);
+
+            //((SubmainActivity)SubmainActivity.mContext).createTextView(name,year+"-"+monthOfYear+"-"+dayOfMonth);
         }
     };
+    public void openDatabase(String databaseName)
+    {
+        database = openOrCreateDatabase(databaseName,MODE_PRIVATE,null);
+    }
+    public void createTable(String tableName)
+    {
+
+        if(database != null)
+        {
+            String sql = "Create Table IF NOT EXISTS " + tableName+ " (_id integer primary key autoincrement, name text, date text)";
+            database.execSQL(sql);
+        }
+    }
+    public void insertData(String name, String date,String tablename)
+    {
+        if(database!=null)
+        {
+            String sql = "insert into " + tablename+ "(name, date) values(?, ?)";
+            Object[] params = {name, date};
+            database.execSQL(sql,params);
+        }
+    }
+    public void selectData(String tableName)
+    {
+
+        if(database!=null)
+        {
+            String str ="";
+            String sql = "select name, date from " + tableName;
+            Cursor cursor = database.rawQuery(sql,null);
+            for(int i = 0; i <cursor.getCount();i++)
+            {
+                cursor.moveToNext();
+                String name = cursor.getString(0);
+                String date = cursor.getString(1);
+
+                str = str + "#"+i+" -> "+name+", "+date+"/n";
+            }
+            Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
+        }
+    }
 }
--- app/src/main/java/com/example/andoridproject/SubmainActivity.java
+++ app/src/main/java/com/example/andoridproject/SubmainActivity.java
@@ -2,13 +2,40 @@ package com.example.andoridproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubmainActivity extends AppCompatActivity {
+    public static Context mContext;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    int tableCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_main);
+        textView1 = findViewById(R.id.textTable1);
+        textView2 = findViewById(R.id.textTable2);
+        textView3 = findViewById(R.id.textTable3);
+        textView4 = findViewById(R.id.textTable4);
+        textView5 = findViewById(R.id.textTable5);
+        mContext = this;
+    }
+    public void createTextView(String name, String date)
+    {
+        switch(tableCount)
+        {
+            case 0 : textView1.setCursorVisible(true);textView1.setText(name+"/n"+date);tableCount++;break;
+            case 1 : textView2.setCursorVisible(true);textView2.setText(name+"/n"+date);tableCount++;break;
+            case 2 : textView3.setCursorVisible(true);textView3.setText(name+"/n"+date);tableCount++;break;
+            case 3 : textView4.setCursorVisible(true);textView4.setText(name+"/n"+date);tableCount++;break;
+            case 4 : textView5.setCursorVisible(true);textView5.setText(name+"/n"+date);tableCount++;break;
+
+        }
     }
 }
