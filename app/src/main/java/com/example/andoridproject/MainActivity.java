2c5fcce90534f03581830984a200e2d22461e378
kimuihyeon222 <rladml91gus@naver.com>
2019-11-12 22:35:44 +0900

    음성등록 구현 구글 음성기능 입력받고 DB로 저장

--- app/src/main/java/com/example/andoridproject/MainActivity.java
+++ app/src/main/java/com/example/andoridproject/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Context;
 import android.content.Intent;
@@ -15,18 +16,26 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -43,17 +52,22 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private View drawerView;
 
+    //의현
+    private static final int REQUEST_CODE = 1234;
+    ArrayList<String> matches_text;
+    //의현
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CONTEXT =this;
+        CONTEXT = this;
         mainbut = findViewById(R.id.mainbutton);
         layer = new ImageButton[10];
         layer[0] = findViewById(R.id.layer1);
         layer[1] = findViewById(R.id.layer2);
         layer[2] = findViewById(R.id.layer3);
-        layer[3]= findViewById(R.id.layer4);
+        layer[3] = findViewById(R.id.layer4);
         layer[4] = findViewById(R.id.layer5);
         layer[5] = findViewById(R.id.layer6);
         layer[6] = findViewById(R.id.layer7);
@@ -76,12 +90,32 @@ public class MainActivity extends AppCompatActivity {
                     String date = cursor.getString(1);
                     sql = "DELETE FROM FOOD WHERE name = '" + name + "' AND date = '" + date + "';";
                     db.execSQL(sql);
-                    Toast.makeText(getApplicationContext(),"먹었어요~",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "먹었어요~", Toast.LENGTH_SHORT).show();
                 }
                 db.close();
                 onResume();
             }
         });
+
+        //의현 - 음성등록
+        soundbut = findViewById(R.id.soundbutton);
+
+        soundbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isConnected()) {
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    startActivityForResult(intent, REQUEST_CODE);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Plese Connect to Internet", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+        //의현
+
         //직접등록버튼 입력 -> 달력(유통기한설정)
         directbut = findViewById(R.id.directbutton);
         directbut.setOnClickListener(new View.OnClickListener() {
@@ -131,15 +165,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SubmainActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        //음성등록버튼 작업
-        soundbut = findViewById(R.id.soundbutton);
-        soundbut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //mDialog.show();
             }
         });
 
@@ -198,32 +223,99 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    //의현
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo net = cm.getActiveNetworkInfo();
+        if (net != null && net.isAvailable() && net.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data1) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            matches_text = data1
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            //Toast.makeText(getApplicationContext(),matches_text.get(position),Toast.LENGTH_LONG).show();
+            String text = matches_text.get(0).replace(" ", "");
+            String name = "", data = "";
+            String[] final_arr;
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) >= '0' && text.charAt(i) <= '9') {
+                    data = text.substring(i, text.length() - 1);
+                    i = text.length() - 1;
+                } else { //음식이름
+                    name += text.charAt(i);
+                }
+            }
+            data = data.replace("년", "-");
+            data = data.replace("월", "-");
+            Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+            String[] temp_data = data.split("-");
+            String date = ""; //최종 년-월-일
+
+            if (name == "" || data == "")
+                Toast.makeText(getApplicationContext(), "입력이 잘못되었습니다.", Toast.LENGTH_LONG).show();
+            else {
+                if ((Integer.parseInt(temp_data[0]) >= 2019) && (Integer.parseInt(temp_data[0]) < 2050)) {
+                    if (Integer.parseInt(temp_data[1]) < 13) {
+                        if (Integer.parseInt(temp_data[2]) < 32) {
+                            if (Integer.parseInt(temp_data[1]) < 10)
+                                temp_data[1] = "0" + temp_data[1]; //월
+                            if (Integer.parseInt(temp_data[2]) < 10)
+                                temp_data[2] = "0" + temp_data[2]; //일
+                            date = temp_data[0] + "-" + temp_data[1] + "-" + temp_data[2];
+                            final_arr = new String[]{name, date};
+                            insertDB(final_arr);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "요일이 되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "월이 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "년도가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data1);
+    }
+    //의현
+
     //달력출력및 DB추가
     private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            monthOfYear+=1;               //월이 1작아서 1추가
-            String day = ""+dayOfMonth;
-            String month = ""+monthOfYear;
-            if(dayOfMonth < 10) {          //YYYY-MM-DD 형식으로 맞추기위해
+            monthOfYear += 1;               //월이 1작아서 1추가
+            String day = "" + dayOfMonth;
+            String month = "" + monthOfYear;
+            if (dayOfMonth < 10) {          //YYYY-MM-DD 형식으로 맞추기위해
                 day = "0" + dayOfMonth;
             }
-            if(monthOfYear < 10)
+            if (monthOfYear < 10)
                 month = "0" + monthOfYear;
-            String date = year+"-"+month+"-"+day;
-            DBHelper helper = new DBHelper(CONTEXT);
-            SQLiteDatabase db = helper.getWritableDatabase();
-            db.execSQL("INSERT INTO FOOD (name, date) VALUES (?, ?)",new String[]{name,date});
-            String sql = "SELECT name, date FROM FOOD ORDER BY date ASC";
-            Cursor cursor = db.rawQuery(sql, null);
-            cursor.moveToNext();
-            mainbut.setText(cursor.getString(0) + "\n" + cursor.getString(1));
-            Toast.makeText(getApplicationContext(),"음식이 등록되었습니다~",Toast.LENGTH_SHORT).show();
-            db.close();
-            setGage();
-            setMainbut();
+            String date = year + "-" + month + "-" + day;
+            String[] arr = new String[]{name, date};
+            insertDB(arr);
         }
     };
+
+    public void insertDB(String[] arr) {
+        DBHelper helper = new DBHelper(CONTEXT);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL("INSERT INTO FOOD (name, date) VALUES (?, ?)", new String[]{arr[0], arr[1]});
+        String sql = "SELECT name, date FROM FOOD ORDER BY date ASC";
+        Cursor cursor = db.rawQuery(sql, null);
+        cursor.moveToNext();
+        mainbut.setText(cursor.getString(0) + "\n" + cursor.getString(1));
+        Toast.makeText(getApplicationContext(), "음식이 등록되었습니다~", Toast.LENGTH_SHORT).show();
+        db.close();
+        setGage();
+        setMainbut();
+    }
 
     //화면 새로고침
     @Override
@@ -233,98 +325,96 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase db = helper.getWritableDatabase();
         String sql = "SELECT name, date FROM FOOD ORDER BY date ASC";
         Cursor cursor = db.rawQuery(sql, null);
-        if(cursor.getCount()!=0) {
+        if (cursor.getCount() != 0) {
             cursor.moveToNext();
             mainbut.setText(cursor.getString(0) + "\n" + cursor.getString(1));
-        }
-        else
+            mainbut.setGravity(Gravity.LEFT);
+        } else {
             mainbut.setText("음식이 없어요!");
+            mainbut.setGravity(Gravity.CENTER);
+        }
         db.close();
         setGage();
         setMainbut();
     }
+
     //MainButton 구성
-    public void setMainbut()
-    {
-        mainbut.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.design_button));
-        mainbut.setTextColor(Color.parseColor("#000000"));
+    public void setMainbut() {
+        mainbut.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.main_button));
         DBHelper helper = new DBHelper(CONTEXT);
         SQLiteDatabase db = helper.getWritableDatabase();
         String sql = "SELECT name, date FROM FOOD ORDER BY date ASC";
         Cursor cursor = db.rawQuery(sql, null);
-        if(cursor.getCount()!=0) {
+        if (cursor.getCount() != 0) {
             cursor.moveToNext();
-            mainbut.setText(cursor.getString(0) + "\n" + cursor.getString(1));
-            long now  = System.currentTimeMillis();
+            mainbut.setText("\n   "+cursor.getString(0)+"          \n\n                 D-DAY\n           "+cursor.getString(1));
+            long now = System.currentTimeMillis();
+            mainbut.setGravity(Gravity.LEFT);
             Date mdate = new Date(now);
             SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd");
             String getTime = simpleDate.format(mdate);
-            getTime = getTime.replace("-","");
+            getTime = getTime.replace("-", "");
             int currentTime = Integer.parseInt(getTime);
             String date = cursor.getString(1);
-            date = date.replace("-","");
+            date = date.replace("-", "");
             int database = Integer.parseInt(date);
-            int limit_time = database-currentTime;
-            if(limit_time<0) {
-                mainbut.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.red_button));
+            int limit_time = database - currentTime;
+            if (limit_time < 0) {
+                mainbut.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.red_button));
                 mainbut.setTextColor(Color.parseColor("#FFFFFF"));
             }
         }
         db.close();
     }
+
     //Gage구성
-    public void setGage()
-    {
+    public void setGage() {
         int count = 0; //아이템 개수
-        long now  = System.currentTimeMillis();
+        long now = System.currentTimeMillis();
         Date mdate = new Date(now);
         SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd");
         String getTime = simpleDate.format(mdate);
-        getTime = getTime.replace("-","");
+        getTime = getTime.replace("-", "");
         int currentTime = Integer.parseInt(getTime);
         DBHelper helper = new DBHelper(CONTEXT);
         SQLiteDatabase db = helper.getWritableDatabase();
         String sql = "SELECT name, date FROM FOOD ORDER BY date DESC";
         Cursor cursor = db.rawQuery(sql, null);
         //초기화
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             layer[i].setBackgroundColor(0XCCCCCCCC);
-            if(i==0)
-                layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.nomal_bottom));
-            else if(i==9)
-                layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.nomal_top));
+            if (i == 0)
+                layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.nomal_bottom));
+            else if (i == 9)
+                layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.nomal_top));
         }
         //색지정
         for (int i = 0; i < cursor.getCount(); i++) {
             cursor.moveToNext();
             String date = cursor.getString(1);
-            date = date.replace("-","");
+            date = date.replace("-", "");
             int database = Integer.parseInt(date);
-            int limit_time = database-currentTime;
-            if(limit_time<0&&count<10) {
+            int limit_time = database - currentTime;
+            if (limit_time < 0 && count < 10) {
                 layer[i].setBackgroundColor(0XFFF44336);
-                if(count==0)
-                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.red_bottom));
-                else if(count==9)
-                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.red_top));
+                if (count == 0)
+                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.red_bottom));
+                else if (count == 9)
+                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.red_top));
                 count++;
-            }
-            else if(limit_time<=4 && count<10)
-            {
+            } else if (limit_time <= 4 && count < 10) {
                 layer[i].setBackgroundColor(0xFFFFEB3B);
-                if(count==0)
-                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.yellow_bottom));
-                else if(count==9)
-                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.yellow_top));
+                if (count == 0)
+                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.yellow_bottom));
+                else if (count == 9)
+                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.yellow_top));
                 count++;
-            }
-            else if(count<10)
-            {
+            } else if (count < 10) {
                 layer[i].setBackgroundColor(0XFF4CAF50);
-                if(count==0)
-                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.green_bottom));
-                else if(count==9)
-                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.green_top));
+                if (count == 0)
+                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.green_bottom));
+                else if (count == 9)
+                    layer[i].setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.green_top));
                 count++;
             }
         }
